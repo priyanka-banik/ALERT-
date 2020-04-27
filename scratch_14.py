cee1f37654131bafda4fae8fe370c15eae25f601
priyanka-banik <64273307+priyanka-banik@users.noreply.github.com>
2020-04-27 20:24:47 +0530

    Update scratch_14.py

--- scratch_14.py
+++ scratch_14.py
@@ -4,6 +4,7 @@ import csv
 from tkinter import messagebox
 window=tk.Tk()
 window.config(background="black")
+
 Label(window,text="COVID-19 HOTSPOT AREA ALERT",font=("Algerian",24,"bold"),bg="black",fg="red").pack()
 entry=tk.Entry(window)
 entry.pack()
@@ -25,15 +26,32 @@ def button_click():
                 k=int(k)+int(txt[8])
         if count!=0:
             if k==0:
-                messagebox.showinfo("Alert",t+" is a safe area with"+str(k)+"cases")
+                answer1=messagebox.showinfo("Alert",t+" is a safe area with"+str(k)+"cases")
+                if(answer1):
+                    window.quit()
             if k<15:
-                messagebox.showinfo("Alert!!",t+" is under moderate danger.\nYou are in orange zone.\nThere are total"+str(k)+"cases.")
+                answer2=messagebox.showinfo("Alert!!",t+" is under moderate danger.\nYou are in orange zone.\nThere are total"+str(k)+"cases.")
+                if (answer2):
+                    window.quit()
+
             if k>15:
-                messagebox.showinfo("Alert",t+" is having exploding no. of Covid-19 cases.\nYou are in red zone.\nThere are total "+str(k)+"cases.")
+                answer3=messagebox.showinfo("Alert",t+" is having exploding no. of Covid-19 cases.\nYou are in red zone.\nThere are total "+str(k)+"cases.")
+                if (answer3):
+                    window.quit()
+
 label1=tk.Label(window,text="Enter The State You Want To Check In\n#example:Assam,West Bengal,etc...",fg="white",font=("Britannic bold",13,"bold"),bg="black")
 submit_button=Button(window,text="SUBMIT",command=button_click,bg="#E6E914",fg="black").pack()
 #label2=tk.Label(window,text="#example:Assam,West Bengal,etc..i.",font=("Britannic bold",13,),bg="green")
+
 label1.pack()
+
+c=Canvas(window,width=220,height=120,bg='#C0C0C0')
+c.pack()
+
+photo=PhotoImage(file=r"C:\Users\acer\Pictures\Screenshots\Screenshot (62).png")
+c.create_image(0,0,image=photo,anchor=NW)
+
 #label2.pack()
 tk.Label(window,text="Stay at home,a wise man said.\n Vijay did'nt obey this and went out like a nicompoop.\nHe is dead now.\nDon't be like Vijay.",font=("chiller",40,),bg="black",fg="#E6E914").pack()
+
 window.mainloop()
